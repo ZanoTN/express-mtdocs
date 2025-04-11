915e81ddc1ab78061314ca9704e6777d89adb4b2
Riccardo Zanotelli <riccardo.zanotelli@gmail.com>
2025-04-11 16:31:54 +0200

    Move to ejs

--- index.js
+++ index.js
@@ -1,3 +1,39 @@
-const { view } = require('./lib/webpage');
+const express = require('express');
+const path = require('path');
+const ejs = require('ejs');
 
-exports.view = view;
+const { getMenu, getSectionHtml } = require('./lib/data');
+
+
+const router = express.Router();
+
+// Serve static files from package's public directory
+router.use('/_rtdocs_assets', express.static(path.join(__dirname, 'public')));
+
+// Render EJS template from inside the module
+router.get('*', (req, res) => {
+  const templatePath = path.join(__dirname, 'views', 'layout.ejs');
+
+  const sectionHtml = getSectionHtml(req);
+  const menuList = getMenu(req);
+
+  const data = {
+    sectionHtml,
+    menuList,
+    baseUrl: '/rtdocs',
+    title: 'RTDocs'
+  };
+
+  ejs.renderFile(templatePath, data, (err, html) => {
+    if (err) {
+      console.error('Error rendering EJS template:', err);
+      res.status(500).send('Internal Server Error');
+    } else {
+      res.send(html);
+    }
+  });
+});
+
+module.exports = {
+  view: router
+};
